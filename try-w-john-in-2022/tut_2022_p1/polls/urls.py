e119cdf3c07339d634495d14c6107896975d0c80
Jim <jim@jam.sessionsnet.org>
2022-02-22 02:20:30 -0800

    connect url pattern to results view of a question

--- try-w-john-in-2022/tut_2022_p1/polls/urls.py
+++ try-w-john-in-2022/tut_2022_p1/polls/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
 
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
+
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
 ]
 
