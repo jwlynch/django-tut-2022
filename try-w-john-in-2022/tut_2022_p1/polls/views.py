ef979861878ccb69374f2b4a5dde552f9deeeb8d
Jim <jim@jam.sessionsnet.org>
2022-02-22 05:07:26 -0800

    put string arg to HttpResponse into output, and output as arg to HttpResponse

--- try-w-john-in-2022/tut_2022_p1/polls/views.py
+++ try-w-john-in-2022/tut_2022_p1/polls/views.py
@@ -9,7 +9,9 @@ def index(request):
     return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    output = "You're looking at question %s." % question_id
+
+    return HttpResponse(output)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
